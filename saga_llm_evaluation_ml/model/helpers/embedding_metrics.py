04d50ff33b9dd4acf9740b38a159208d5ccce94d
Lucie <lucie.navez@sagacify.com>
2023-10-17 15:18:04 +0200

    feat(embedding_metrics): implement BERTScore and MAUVE and add unit tests support

---
+++ saga_llm_evaluation_ml/model/helpers/embedding_metrics.py
@@ -0,0 +1,60 @@
+# TODO: Implement BERTScore
+# TODO: Implement MAUVE
+from evaluate import load
+
+
+class BERTScore:
+    def __init__(self, model_type="roberta-large"):
+        """
+        BERTScore computes a similarity score for each token in the candidate sentence with each token in the reference sentence.
+        The final score is the average of the similarity scores of all tokens in the candidate sentence.
+
+        Args:
+            model_type (str, optional): Model type to use. Defaults to "roberta-large".
+        """
+        self.model_type = model_type
+        self.metric = load("bertscore")
+
+    def compute(self, references, predictions, **kwargs):
+        """
+        Args:
+            references (list): List of reference sentences.
+            predictions (list): List of candidate sentences.
+
+        Returns:
+            list: List of scores for each candidate sentence. Contains a list of scores for precisions, recalls, and F1 scores.
+        """
+        assert len(references) == len(
+            predictions
+        ), "Number of references and predictions must be equal."
+        assert isinstance(references, list), "References must be a list."
+        assert isinstance(predictions, list), "Predictions must be a list."
+
+        return self.metric.compute(
+            predictions=predictions,
+            references=references,
+            model_type=self.model_type,
+            **kwargs
+        )
+
+
+class MAUVE:
+    def __init__(self):
+        """
+        MAUVE score computes the difference between the candidate sentence distribution and the reference sentence distribution.
+        The bigger the MAUVE score, the better.
+        """
+        self.metric = load("mauve")
+
+    def compute(self, references, predictions, **kwargs):
+        """
+        Args:
+            references (list): List of reference sentences.
+            predictions (list): List of candidate sentences.
+
+        Returns:
+            list: List of MAUVE scores for each candidate sentence.
+        """
+        return self.metric.compute(
+            predictions=predictions, references=references, **kwargs
+        )
---
+++ tests/test_embedding_metrics.py
@@ -0,0 +1,32 @@
+import unittest
+
+from saga_llm_evaluation_ml.model.helpers.embedding_metrics import BERTScore, MAUVE
+
+
+class TestBERTScore(unittest.TestCase):
+    def test_compute(self):
+        """Tests that the BERTScore class computes the correct scores. And that the scores are the same when the same inputs are given."""
+        references = ["The cat sat on the mat.", "The dog sat on the log."]
+        predictions = ["The cat sat on the mat.", "The dog sat on the log."]
+        bertscore = BERTScore()
+        scores = bertscore.compute(references, predictions)
+        print(scores)
+        self.assertEqual(len(scores["precision"]), len(references))
+        self.assertEqual(len(scores["recall"]), len(references))
+        self.assertEqual(len(scores["f1"]), len(references))
+
+        scores_2 = bertscore.compute(references, predictions)
+        self.assertEqual(scores["precision"], scores_2["precision"])
+        self.assertEqual(scores["recall"], scores_2["recall"])
+        self.assertEqual(scores["f1"], scores_2["f1"])
+
+
+class TestMAUVE(unittest.TestCase):
+    def test_compute(self):
+        """Tests that the MAUVE class computes the same scores when the same inputs are given."""
+        mauve = MAUVE()
+        references = ["The cat sat on the mat.", "The dog sat on the log."]
+        predictions = ["The cat sat on the mat.", "The dog sat on the log."]
+        scores = mauve.compute(references, predictions)
+        scores_2 = mauve.compute(references, predictions)
+        self.assertEqual(scores.mauve, scores_2.mauve)
